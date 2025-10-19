781b54bec1dbeb009bf67843fb172b8bbf340040
milan <milanzivanov@users.noreply.github.com>
2025-10-19 12:04:42 +0200

    add draftMode and add generateStaticParams

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,13 +1,13 @@
 import { draftMode } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { redirect } from "next/navigation";
 
-export const GET = async (request: NextResponse) => {
+export const GET = async (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
   const slug = searchParams.get("slug") || "/";
 
   if (!slug) {
-    return NextResponse.json({ message: "Slug is required" }, { status: 400 });
+    redirect("/");
   }
 
   (await draftMode()).enable();
